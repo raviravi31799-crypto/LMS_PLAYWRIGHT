3f7c870830e9c154817c1965a5c17a4ce9265167
Subhashree <subhashreerajait@gmail.com>
2026-07-08 06:44:00 +0530

    added loggers and reports

--- src/test/pages/EditDeletepage.ts
+++ src/test/pages/EditDeletepage.ts
@@ -8,16 +8,18 @@ export class EditDeletepage extends Basepage {
     }
     cousemodule=this.page.locator("//div[@title='Course Management']//div[@class='p-1.5']//*[name()='svg']");
     editCourse = this.page.locator("//button[normalize-space()='Edit Course']");
-    nextButton = this.page.locator("//button[normalize-space()='Next']");
+    nextButton = this.page.locator("//div[@role='dialog']//button[normalize-space()='Next']");
     previewUpdateButton = this.page.locator("//button[contains(.,'Preview') and contains(.,'Update')]");
     saveCourseLayoutButton = this.page.locator("//button[contains(.,'Save Course Layout')]");
-    successMessage = this.page.locator("//div[contains(@class,'Toastify') or contains(text(),'success')]");
+    successMessage = this.page.locator('.Toastify__progress-bar--wrp');
     laterButton = this.page.locator('button:has-text("Later")');
     viewButton = this.page.locator("(//button[contains(.,'View')])[1]");
-    courseList = this.page.locator("//h4[normalize-space()='Course Levels']/parent::div/following-sibling::div//span");
-    subModuleCheckbox = this.page.locator("//input[@id='submodule-checkbox']");
+    courseList = this.page.locator("//h4[normalize-space()='Course Levels']/parent::div/following-sibling::div//span"); 
+    subModuleCheckbox = this.page.locator("label[for='submodule-checkbox']:visible");
     topicCheckbox = this.page.locator("//input[@id='topic-checkbox']");
     subTopicCheckbox = this.page.locator("//input[@id='subtopic-checkbox']");
+    loadingSpinner = this.page.locator("//div[contains(text(),'Loading course data')]");
+    modalLoadingOverlay = this.page.locator("//div[@role='dialog']//div[contains(@class,'flex-1') and contains(@class,'items-center') and contains(@class,'justify-center')]");
  async navigatecoursemodule(){
        await this.click(this.cousemodule);
   }
@@ -35,16 +37,18 @@ export class EditDeletepage extends Basepage {
         await this.click(this.editCourse);
     }
 
-    async clickNextButton() {
-        await this.nextButton.waitFor({ state: "visible" });
-        await this.nextButton.click({ force: true });
-    }
-
-    async editCourseHierarchy() {
+  async clickNextButton() {
+    await this.nextButton.waitFor({ state: "visible" });
+    await this.modalLoadingOverlay.waitFor({ state: "hidden", timeout: 30000 }).catch(() => {});
+    await this.nextButton.click();
+}
+async editCourseHierarchy() {
+    await this.loadingSpinner.waitFor({ state: "hidden", timeout: 30000 });
     await this.checkbox(this.subModuleCheckbox);
     await this.checkbox(this.topicCheckbox);
     await this.checkbox(this.subTopicCheckbox);
-   }
+}
+    
     async clickPreviewUpdate() {
         await this.click(this.previewUpdateButton);
     }
